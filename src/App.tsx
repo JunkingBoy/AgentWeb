62563b7f38ddfa47755f3993bd58178a33bed9ba
junkingboy <junkingboy@163.com>
2026-08-31 15:35:09 +0800

    [fix] 挂载全局 Toaster 容器与 Toast/Popover 配色(修复 toast 不显示;仅合并 lucifer-dev 非文件上传部分)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'next-themes'
 import router from '@/routes'
+import { Toaster } from '@/components/ui/sonner'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       disableTransitionOnChange={false}
     >
       <RouterProvider router={router} />
+      {/* 全局 Toast 容器（此前未挂载，导致所有 toast 不显示） */}
+      <Toaster position="top-center" richColors closeButton />
     </ThemeProvider>
   )
 }
